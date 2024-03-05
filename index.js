682e82e067f2a099cbaeec73c62737a3bdcacca8
lossborn <necessary.dire@gmail.com>
2024-03-05 20:55:11 +0300

    temp

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
     'ServerFactory': require('./lib/server-factory'),
-    'HttpSms': require('./lib/sms/http-sms'),
-    'SocketSms': require('./lib/sms/socket-sms'),
+    'HttpSms': require('./lib/modules/http-sms'),
+    'SocketSms': require('./lib/modules/socket-sms'),
+    'SocketUssd': require('./lib/modules/socket-ussd'),
     'MessageDispatcher': require('./lib/message-dispatcher'),
     'MessageFactory': require('./lib/message-factory'),
 }
